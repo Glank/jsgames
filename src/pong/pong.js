c266b9976514cff3db71cf59e8a5dc2cf18ed05a
Ernest Kirstein <glank314@gmail.com>
2021-10-22 22:22:12 -0400

    Implemented tests for pong.

--- src/pong/pong.js
+++ src/pong/pong.js
@@ -1,7 +1,50 @@
-var newBall = function(screen_width, screen_height) {
+function initBall(game_display) {
 	var ball = {
-		'x': Math.floor(screen_width/2),
-		'y': Math.floor(screen_height/2),
+		'x': game_display.width/2.0,
+		'y': game_display.height/2.0,
+		'speed': 50 // pixels per second
 	};
+	if(Math.random() < 0.5) {
+		ball.angle = 0;
+	} else {
+		ball.angle = Math.PI;
+	}
 	return ball;
+}
+
+function updateBall(ball, dt, game_display) {
+	var dx = ball.speed*Math.cos(ball.angle);
+	var dy = ball.speed*Math.sin(ball.angle);
+	ball.x += dx*dt;
+	ball.y += dy*dt;
+
+	// handle bouncing off walls
+	var bounced = false;
+	if(ball.x < 0) {
+		ball.x = 0;
+		dx *= -1;
+		bounced = true;
+	}
+	if(ball.x > game_display.width) {
+		ball.x = game_display.width;
+		dx *= -1;
+		bounced = true;
+	}
+	if(ball.y < 0) {
+		ball.y = 0;
+		dy *= -1;
+		bounced = true;
+	}
+	if(ball.y > game_display.height) {
+		ball.y = game_display.height;
+		dy *= -1;
+		bounced = true;
+	}
+	
+	ball.angle = Math.atan2(dy, dx);
+	if(bounced) {
+		if (ball.speed < 200)
+			ball.speed += 1;
+		ball.angle += (Math.random()-0.5)/2;
+	}
 }
--- test/pong.js
+++ test/pong.js
@@ -1,6 +1,20 @@
 #include src/test_utils.js
 #include src/pong/pong.js
 
-ball = newBall(10, 5);
-assert(ball.x === 5, 'ball.x should be 5');
-assert(ball.y === 2, 'ball.y should be 2');
+function testBall() {
+  display = {
+    'width': 200,
+    'height': 100
+  }
+  var ball = initBall(display);
+  assert(ball.x === 100, 'ball.x should be 100');
+  assert(ball.y === 50, 'ball.y should be 50');
+
+  ball.angle = 0;
+  updateBall(ball, 1.0, display);
+  var expected_x = 100+ball.speed;
+  assert(ball.x === expected_x, 'ball.x should be '+expected_x+' not '+ball.x);
+}
+
+testBall();
+
