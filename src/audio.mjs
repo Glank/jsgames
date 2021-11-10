4855399a60ab50ae4ac1fc1e78c3450ccb509111
Ernest Kirstein <glank314@gmail.com>
2021-11-10 14:05:43 -0500

    Encapsulated audio demo into audio library.

--- src/audio.mjs
+++ src/audio.mjs
@@ -1,31 +1,47 @@
-function preloadedSound(src) {
-  var this_ = {};
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', src, false);
-	var src_buf = src;
-	xhr.onload = function () {
-			var blob = new Blob(Uint8Array.from(xhr.response, c => c.charCodeAt(0)), {type:'audio/mpeg'});
-			src_buf = URL.createObjectURL(blob);
-	};
-	xhr.send();
-  this_.sound = document.createElement("audio");
-  this_.sound.src = src;
-  this_.sound.setAttribute("preload", "auto");
-  this_.sound.setAttribute("controls", "none");
-  this_.sound.style.display = "none";
-  document.body.appendChild(this_.sound);
-  this_.play = function(){
-    this_.sound.currentTime = 0;
-    this_.sound.play();
-  }
-  return this_;
-}
-
-class AudioManager {
+export class AudioManager {
   constructor() {
-    this.sounds = {};
+    this.effects = {};
+  	this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+		this.game = null;
   }
-  play(sound) {
-    // TODO
+	loadEffect(name, src) {
+		var effect = {
+			data:null,
+			source:null
+		};
+		var this_ = this;
+		effect.promis = fetch(src)
+			.then(response => response.arrayBuffer())
+			.then(buf => this.audioCtx.decodeAudioData(buf))
+			.then(data => effect.data = data)
+			.catch(function(err) { 
+				if (this_.game) {
+					game.debug.error_message = err.message;
+					game.debug.error_stack= err.stack;
+				}
+				console.log(err);
+			});
+		this.effects[name] = effect;
+	}
+  playEffect(name) {
+		var effect = this.effects[name];
+		if (!effect.data)
+			return;
+    if (effect.source !== null) {
+      try {
+        effect.source.disconnect(this.audioCtx.destination);
+        effect.source.stop(this.audioCtx.currentTime);
+      } catch(err) {
+				if (this.game) {
+					game.debug.error_message = err.message;
+					game.debug.error_stack= err.stack;
+				}
+        console.log(err);
+      }
+    }
+    effect.source = this.audioCtx.createBufferSource();
+    effect.source.buffer = effect.data;
+    effect.source.connect(this.audioCtx.destination);
+    effect.source.start(this.audioCtx.currentTime);
   }
 }
